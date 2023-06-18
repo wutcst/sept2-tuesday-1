08e9faff89518e0973a1aba5bcd099de975fa8a4
zengzidi <1942940786@qq.com>
2023-06-18 13:45:20 +0800

    check_acc_V1

---
+++ src/main/java/cn/edu/whut/sept/zuul/config/SpringMVCConfig.java
@@ -0,0 +1,17 @@
+package cn.edu.whut.sept.zuul.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMVCConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowCredentials(true)
+                .allowedMethods("*")
+                .maxAge(3600);
+    }
+}
